7a9e7a3cef1b47c4e1bc42fffec3569efa65ba13
Carlll562 <213318798+Carlll562@users.noreply.github.com>
2026-01-16 21:54:24 +0800

    changes

--- home/js/vue.js
+++ home/js/vue.js
@@ -9,20 +9,3 @@
   
   app.mount('#app')
 
-  const apps = Vue.createApp({
-   data() {
-    return {
-      manyFoods: [
-        'https://www.w3schools.com/vue/img_burrito.svg',
-        'https://www.w3schools.com/vue/img_salad.svg',
-        'https://www.w3schools.com/vue/img_cake.svg',
-        'https://www.w3schools.com/vue/img_soup.svg',
-        'https://www.w3schools.com/vue/img_fish.svg',
-        'https://www.w3schools.com/vue/img_pizza.svg',
-        'https://www.w3schools.com/vue/img_rice.svg'
-      ]
-    }
-   }
-  })
-
-  app.mount('#apps ')
