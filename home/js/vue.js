89cc0a74630ee5adeff6bb9f82b9126f0e96337b
Carlll562 <213318798+Carlll562@users.noreply.github.com>
2026-01-16 21:43:07 +0800

    added food images

--- home/js/vue.js
+++ home/js/vue.js
@@ -8,3 +8,21 @@
   })
   
   app.mount('#app')
+
+  const apps = Vue.createApp({
+   data() {
+    return {
+      manyFoods: [
+        'img_burrito.svg',
+        'img_salad.svg',
+        'img_cake.svg',
+        'img_soup.svg',
+        'img_fish.svg',
+        'img_pizza.svg',
+        'img_rice.svg'
+      ]
+    }
+   }
+  })
+
+  app.mount('#apps ')
